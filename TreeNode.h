0917ac7171e8e94fb9b60da61e7117ecc6851878
Maciej Suchocki <M.Suchocki@stud.elka.pw.edu.pl>
2016-12-10 21:46:05 +0100

    TreeNode.h DONE

--- TreeNode.h
+++ TreeNode.h
@@ -9,8 +9,6 @@ struct TreeNode
     TreeNode<KeyType, ValueType>* left_son;
     TreeNode<KeyType, ValueType>* right_son;
     std::pair<const KeyType, ValueType> pair;
-   // BSTNode(BSTNode* p):parent(p), left(nullptr), right(nullptr){
-   // }
     TreeNode():parent(nullptr),left_son(nullptr), right_son(nullptr) {}
     TreeNode(KeyType key, ValueType value) : parent(nullptr), left_son(nullptr), right_son(nullptr), pair(std::make_pair(key, value)) {}
 
